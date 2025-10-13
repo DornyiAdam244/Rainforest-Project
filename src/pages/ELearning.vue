96f54dc187a7fe62dfe4c536150cc90dd8990164
dzsekkk <94841029+DornyiAdam244@users.noreply.github.com>
2025-10-13 11:35:38 +0200

    Resolve routing issues

---
+++ src/pages/ELearning.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  
+  <main style="max-width:1100px;margin:0 auto;padding:1rem">
+    <router-view />
+  </main>
+  <Footer />
+</template>
---
+++ src/pages/Quiz.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  
+  <main style="max-width:1100px;margin:0 auto;padding:1rem">
+    <router-view />
+  </main>
+  <Footer />
+</template>
