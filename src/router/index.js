67a02ad3f7d2b8e1fc11676ebc561f2c9906b480
Hovanyecz István Dániel <hovanyecz.istvan.daniel.2021i@bankitatabanya.hu>
2025-10-15 19:47:19 +0200

    Added Route to User Registry Page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Quiz from '../pages/Quiz.vue';
 import ELearning from '../pages/ELearning.vue';
+import Registry from '../pages/Registry.vue';
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -10,7 +11,8 @@ const router = createRouter({
     { path: '/', name: 'home', component: Home },
     { path: '/quiz', name: 'quiz', component: Quiz },
     { path: '/elearning', name: 'elearning', component: ELearning },
-    { path: '/:pathMatch(.*)*', redirect: '/' }
+    { path: '/:pathMatch(.*)*', redirect: '/' },
+    {path: '/registry', name: 'registry', component: Registry}
   ],
   scrollBehavior: () => ({ top: 0 })
 });
